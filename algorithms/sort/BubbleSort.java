4d92491209584fc6bedc819d4a7821703ce14067
khlongwa023 <khlongwa023@student.wethinkcode.co.za>
2024-06-20 11:11:39 +0200

    remove unnecessary variables

--- algorithms/sort/BubbleSort.java
+++ algorithms/sort/BubbleSort.java
@@ -21,7 +21,6 @@ public class BubbleSort {
     }
 
     public static void bubbleSort(int[] arr) {
-        int count = 0;
         for (int i = 0; i < arr.length - 1; i++) {
             
             for (int j = 0; j < arr.length - i - 1; j++) {
