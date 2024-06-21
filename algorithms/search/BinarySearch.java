3ce8d80517cd75f478e44ef071073db408a23eec
khlongwa023 <khlongwa023@student.wethinkcode.co.za>
2024-06-21 09:39:09 +0200

    recursive binary search

--- algorithms/search/BinarySearch.java
+++ algorithms/search/BinarySearch.java
@@ -1,7 +1,7 @@
 package algorithms.search;
 
 public class BinarySearch {
-    public static int binarySearch(int[] arr, int target) {
+    private static int binarySearch(int[] arr, int target) {
         
         int low = 0; //lowest index of an array
         int high = arr.length - 1; //highest index of an array
@@ -22,11 +22,34 @@ public class BinarySearch {
         return -1;
     }
 
+    private static int recursiveBinarySearch(int[] arr, int highIndex, int lowIndex, int target) {
+        if (highIndex >= lowIndex) {
+            int mid = (lowIndex + highIndex) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                return recursiveBinarySearch(arr, highIndex, mid + 1, target);
+            }
+            return recursiveBinarySearch(arr, mid - 1, lowIndex, target);
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] arr = new int[]{1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
         int target = 15;
 
         int targetIndex = binarySearch(arr, target);
+        int targetIndexRecursed = recursiveBinarySearch(arr, arr.length - 1, 0, target);
+
+        if (targetIndexRecursed != -1) {
+            System.out.println("Target found by recursion at index " + targetIndexRecursed);
+        } else {
+            System.out.println("Target not found!");
+        }
 
         if (targetIndex != -1) {
             System.out.println("Target found at index " + targetIndex);
